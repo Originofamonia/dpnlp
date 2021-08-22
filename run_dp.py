61247fd15f19c0cd8582cafeb51166b150bf5852
Edward <anqiyuanms@hotmail.com>
2021-08-22 16:14:16 -0500

    parser.add_argument("--task_name", default='tp', type=str,

--- run_dp.py
+++ run_dp.py
@@ -764,10 +764,10 @@ def main():
         help="Path to pre-trained model or shortcut name selected in the list: " + ", ".join(
             ALL_MODELS),
     )
-    parser.add_argument("--task_name", default='ag', type=str,
+    parser.add_argument("--task_name", default='tp', type=str,
         help="The name of the task to train selected in the list: ",
     )
-    parser.add_argument("--output_dir", default='/ag', type=str,
+    parser.add_argument("--output_dir", default='tp', type=str,
         help="The output directory where the model predictions and checkpoints will be written.",
     )
 
